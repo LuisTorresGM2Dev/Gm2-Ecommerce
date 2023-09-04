f106d41101ece16b1e9074eb7f2cefa77bfcb6c8
GM2 Academy <gm2academy@MacBook-Pro-de-Tecnico.local>
2023-09-04 12:56:59 -0300

    feat: init the JavaScript

---
+++ script.js
@@ -0,0 +1,40 @@
+let containerCards;
+const products = [
+    {
+        image:'./images/llaveInglesa.png',
+        name:'Stillson',
+        price: '999',
+        priceDescount:'699',
+        description:'Lorem Ipsum Dolor Sit Amet Consectetur'
+    },
+
+    {
+        image:'./images/llaveInglesa.png',
+        name:'Stillson',
+        price: '999',
+        priceDescount:'699',
+        description: 'Lorem Ipsum Dolor Sit Amet Consectetur'
+    },
+    {
+        image:'./images/llaveInglesa.png',
+        name:'Stillson',
+        price: '999',
+        priceDescount:'699',
+        description: 'Lorem Ipsum Dolor Sit Amet Consectetur'
+    },
+    {
+        image:'./images/llaveInglesa.png',
+        name:'Stillson',
+        price: '999',
+        priceDescount:'699',
+        description: 'Lorem Ipsum Dolor Sit Amet Consectetur'
+    }
+]
+
+function inicializar (){
+    containerCards = document.querySelector(".container-cards");
+}
+window.addEventListener("load", () =>{
+
+
+} )
